b9ef9cb18f55d99ae4a018d6d4df09497bd13de1
Qiang Zhang <qiang@Qiangs-MBP.hsd1.ca.comcast.net>
2020-01-30 21:04:11 -0800

    db test

--- controllers/wordController.js
+++ controllers/wordController.js
@@ -1,11 +1,26 @@
+const mysql = require('mysql');
+
 function wordController() {
+    // DB
+    const db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'pwd',
+        database: 'dictionary'
+    })
+    db.connect((err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('mysql connected.');
+    })
+
     const post = (req, res) => {
         console.log('post');
         console.log(req.body);
         const response = { status: 'success', body: req.body };
         return res.status(201).json(response);
     };
-
     const get = (req, res) => {
         console.log('get')
         // deal with the url query string. for example: /voc?word=test
